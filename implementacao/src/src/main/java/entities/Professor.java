b67046c5afc6f140bc58d798dac04a8e59f8942c
octavio1199 <octaviorocha1199@gmail.com>
2022-08-29 19:55:09 -0300

    Update Professor

--- implementacao/src/src/main/java/entities/Professor.java
+++ implementacao/src/src/main/java/entities/Professor.java
@@ -6,26 +6,33 @@ import java.util.Date;
 public class Professor extends Usuario {
 
 	//#region ATRIBUTOS
-	//#endregion
-
-	//#region CONSTRUCTOR
-	//#endregion
-
-	//#region GETTERS e SETTERS
-	//#endregion
-
-	//#region METODOS ESPECIFICOS
-	//#endregion
-
 	private int codigo_prof;
 	private ArrayList<OfertaDisciplina> ofertas;
-
+	//#endregion
+	
+	//#region CONSTRUCTOR
 	public Professor(String nome, Date data_nascimento, String endereco) {
 		super(nome, data_nascimento, endereco);
 	}
+	//#endregion
 
+	//#region GETTERS e SETTERS
+	public int getCodigo_prof() {
+		return codigo_prof;
+	}
+
+	public ArrayList<OfertaDisciplina> getOfertas() {
+		return ofertas;
+	}
+	//#endregion
+
+	//#region METODOS ESPECIFICOS
 	public ArrayList<Aluno> listarAlunosPorOferta(OfertaDisciplina oferta) {
 		throw new Error("Not implemented");
 	}
+	//#endregion
+
+
+
 
 }
